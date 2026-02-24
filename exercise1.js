9ea42200f4fe237d468f792e51e09350b2dd8952
Zalain Spain <2023159874@ub.edu.bz>
2026-02-23 18:37:59 -0600

    Added comments

--- exercise1.js
+++ exercise1.js
@@ -2,21 +2,27 @@
 
 const accordion = document.querySelector('.accordion');
 
+//attach a single click listeneer to the .accordion element
 accordion.addEventListener('click', function(event) {
          
-  const trigger = event.target.closest('.accordion-trigger');
+  //find the closest trigger from even.target
+  const trigger = event.target.closest('.accordion-trigger'); 
   
+  //if null then the click was not a trigger so we return early
   if (!trigger) return;
 
+  //frin trigger we find the closest .accordion-item
   const item = trigger.closest('.accordion-item');
   const allItems = accordion.querySelectorAll('.accordion-item');
   
+  //close all accordion items
   allItems.forEach(items => {
-    
+
       if(items !== item) {
         items.classList.remove('open');
       }
   });
   
+  //toggle .open on the clicked item
   item.classList.toggle('open');
 });
